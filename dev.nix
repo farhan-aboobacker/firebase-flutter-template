67ca7a8707fe157414da823c6d51b01f705ab9da
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-10-14 13:23:33 +0530

    Create dev.nix

---
+++ dev.nix
@@ -0,0 +1,31 @@
+
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-25.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.jdk21
+    pkgs.unzip
+  ];
+  # Sets environment variables in the workspace
+  env = {
+   
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+       # installDependencies = "flutter pub get";
+      };
+    };
+    # Enable previews and customize configuration
+    
+  };
+}
