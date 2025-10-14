67315f8b1c79cbd9d255e7d0e70cb7b6fff5ee50
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-10-14 13:22:54 +0530

    Create idx-template.nix

---
+++ idx-template.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  packages = [
+   
+  ];
+  bootstrap = ''
+    mkdir -p "$WS_NAME"
+    mkdir "$WS_NAME/.idx/"
+    chmod -R +w "$WS_NAME"
+    cp ${./dev.nix} "$WS_NAME"/.idx/dev.nix
+    mv "$WS_NAME" "$out"
+    mkdir -p "$out/.idx"
+    chmod -R u+w "$out"
+  '';
+}
