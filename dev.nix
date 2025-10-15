4acaddfc8155c002bc152de10bd0675312e6d48c
Farhan-Aboobacker <Farhan.Aboobacker@cognizant.com>
2025-10-15 15:18:35 +0530

    Update dev.nix

--- dev.nix
+++ dev.nix
@@ -1,6 +1,5 @@
-
 # To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
+# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-25.05"; # or "unstable"
@@ -10,9 +9,7 @@
     pkgs.unzip
   ];
   # Sets environment variables in the workspace
-  env = {
-   
-  };
+  env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -21,11 +18,23 @@
     ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
-       # installDependencies = "flutter pub get";
-      };
+      onCreate = { };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
     };
     # Enable previews and customize configuration
-    
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          manager = "flutter";
+        };
+        android = {
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          manager = "flutter";
+        };
+      };
+    };
   };
 }
+ 
